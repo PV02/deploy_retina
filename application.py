1507b3d6748e8f99068a3f64ae19096450c23555
PV02 <94601649+PV02@users.noreply.github.com>
2023-10-27 00:17:56 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,2 @@
-from flask import Flask, render_template, request, jsonify
-import numpy as np
-from keras.models import load_model
-from PIL import Image, ImageOps
 
-print(np.sin(5))
+print("le serveur marche", 5*3)
